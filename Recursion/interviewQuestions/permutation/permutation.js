54b69faef5cebeca29df90cdfaf4a61f5ff7686b
Abhishek Kumar yadav <76660763+Abhishek-1310@users.noreply.github.com>
2026-04-20 15:23:00 +0530

    Update permutation.js

--- Recursion/interviewQuestions/permutation/permutation.js
+++ Recursion/interviewQuestions/permutation/permutation.js
@@ -1,8 +1,8 @@
 var permute = function (nums) {
     let result = [];
 
-    function permu(arr, path, used) {
-        if (path.length === arr.length) {
+    function permu(path, used) {
+        if (path.length === nums.length) {
             result.push([...path]);
             return;
         }
@@ -10,11 +10,11 @@ var permute = function (nums) {
             if (used[i]) continue;
             path.push(arr[i]);
             used[i] = true;
-            permu(arr, path, used);
+            permu(path, used);
             path.pop();
             used[i] = false;
         }
     }
-    permu(nums, [], new Array(nums.length).fill(false));
+    permu([], new Array(nums.length).fill(false));
     return result;
 };
