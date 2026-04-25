570763d8e1301be33e85f039144e4dae5c57abba
Abhishek Kumar yadav <76660763+Abhishek-1310@users.noreply.github.com>
2026-04-25 09:10:19 +0530

    Refactor subseq function to simplify parameters
    Refactor subseq function to remove path parameter and simplify sum calculation.

--- Recursion/Subsequence/PrintSubsequences/printSubs.js
+++ Recursion/Subsequence/PrintSubsequences/printSubs.js
@@ -15,33 +15,32 @@ subseq([1, 2, 3], 0, []);
 
 
 //print sum of all subsequence elemnts
-function subseq(arr, index, path, sum) {
+function subseq(arr, index, sum) {
     if (index === arr.length) {
         console.log(sum);
         return;
     }
-    path.push(arr[index]);
+    
     sum += arr[index]
-    subseq(arr, index + 1, path, sum);
+    subseq(arr, index + 1, sum);
 
-    path.pop();
+
     sum -= arr[index]
-    subseq(arr, index + 1, path, sum);
+    subseq(arr, index + 1, sum);
 }
 
 subseq([1, 2, 3], 0, [], 0);
 
 //print count of subsequence whose sum===k
-function subseq(arr, index, path, sum, k, count) {
+function subseq(arr, index, sum, k, count) {
 
     if (index === arr.length) {
         return sum === k ? 1 : 0;
     }
-    path.push(arr[index]);
-    let left = subseq(arr, index + 1, path, sum + arr[index], k, count);
+    let left = subseq(arr, index + 1, sum + arr[index], k, count);
 
-    path.pop();
-    let right = subseq(arr, index + 1, path, sum, k, count);
+
+    let right = subseq(arr, index + 1, sum, k, count);
 
     return left + right;
 }
